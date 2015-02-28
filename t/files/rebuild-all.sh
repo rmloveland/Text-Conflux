ca58e7064bbca094058539fcbe35da1341d4dbb4
Rich Loveland <r@rmloveland.com>
2015-02-28 08:42:39 -0500

    Remove unhelpful "tests".

--- t/files/rebuild-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Rebuilds all the test files. This should need to be used less often
-# as our formatter becomes more mature.
-
-for f in `ls *.txt`; do
-    confluence2html < $f > $f.html
-done
